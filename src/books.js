4e50d68cf5138678845218dd643374deb5908df3
Nurasyl <nnurasil78@gmail.com>
2025-04-17 13:00:44 +0500

    Test commit

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book.jpg'
+import img2 from './images/book-1.jpg'
+import img3 from './images/book-2.jpg'
+
+export const books = [
+  {
+    author: 'Mel Robbins',
+    title: 'The Let Them Theory',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'Suzanne Collins',
+    title: 'Sunrise on the Reaping',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'James Dean',
+    title: 'Pete the Cat and the Easter Basket Bandit',
+    img: img3,
+    id: 3,
+  },
+]
